3a9213ef43aef8dee3e6596575c5c16c8f205790
Edward_Chiu <gameqube963741@gmail.com>
2020-08-13 15:22:19 +0800

    update

--- practice.py
+++ practice.py
@@ -1,43 +1,13 @@
-# sum = 0 
-# n = int(input('輸入整數:'))
-# for i in range(1, n+1):
-#     sum+=i
-# print('1 到 %d 的整數和 為 %d '%(n,sum))
+# def GetArea(width, height):
+#     area = width * height
+#     return area
 
-# sum = 0
-# n = int(input('輸入正整數:'))
-# for i in range(1, n+1):
-#     sum += i
-# print('1 到 %d 的整數和 為 %d'%(n,sum))
+# ret1 = GetArea(6,9) #ret1 = 54
+# print(ret1)
 
-# n=0
-# for i in range(1,101):
-#     for j in range(1,101):
-#         n += 1
-# print(n)
-
-
-# for i in range(1,10):
-#     for j in range(1,10):
-#         product = i * j
-#         print('%d * %d = %-2d' %(i, j, product),end=' ')
-#     print()
-
-# for i in range(1,11):
-#     if(i == 6):
-#         break
-#     print(i, end = ', ')
-    
-import os
-dir = 'mydir'
-if not os.path.exists(dir):
-    os.mkdir(dir)
-else:
-   print(dir + '資料夾已存在')
-
-import os 
-dir = 'mydir'
-if not os.path.exists(dir):
-    os.mkdir(dir)
-else:
-    print(dir + '資料夾已存在')
+def Circle(radius):
+    area = radius * radius * 3.14
+    length = 2 * radius * 3.14
+    return area, length 
+area1, length1 = Circle(5)
+print(Circle(5))
